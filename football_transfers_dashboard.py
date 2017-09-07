4631b2a8eb6ab58d111755c8a54e1550943630f8
Primovisto <ed@velabri.co>
2017-09-07 23:53:58 +0100

    added code from coursework

--- football_transfers_dashboard.py
+++ football_transfers_dashboard.py
@@ -1,12 +1,49 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'FB_Transfers1234'
+COLLECTION_NAME = 'transfers1234'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route("/")
+def index():
+    """
+    A Flask view to serve the main dashboard page.
+    """
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/donorsUS/projects")
+def donor_projects():
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+
+    # A constant that defines the record fields that we wish to retrieve.
+    FIELDS = {
+        '_id': False, 'WINDOW': True, 'LEAGUE': True,
+        'PRICE': True, 'PRICE DESCRIPTION': True,
+        'POSITION': True, 'SEASON': True, 'COUNTRY': True
+    }
+
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define which collection we wish to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+        projects = collection.find(projection=FIELDS, limit=15000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
