e92d136dbc5237d0d9f1ea54d03cfbee16d5a962
Primovisto <ed@velabri.co>
2017-09-08 01:04:32 +0100

    made adjustments to graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,117 +8,82 @@ function makeGraphs(error, donorsUSProjects) {
         throw error;
     }
 
-    //Clean donorsUSProjects data
-    var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
-    donorsUSProjects.forEach(function (d) {
-        d["date_posted"] = dateFormat.parse(d["date_posted"]);
-        d["date_posted"].setDate(1);
-        d["total_donations"] = +d["total_donations"];
-    });
-
 
     //Create a Crossfilter instance
     var ndx = crossfilter(donorsUSProjects);
 
     //Define Dimensions
     var dateDim = ndx.dimension(function (d) {
-        return d["date_posted"];
+        return d["SEASON"];
     });
-    var resourceTypeDim = ndx.dimension(function (d) {
-        return d["resource_type"];
-    });
-    var povertyLevelDim = ndx.dimension(function (d) {
-        return d["poverty_level"];
+
+    var dateDimChartMargins = {top: 30, right: 50, bottom: 25, left: 90};
+
+    var fundingStatus = ndx.dimension(function (d) {
+        return d["WINDOW"];
     });
     var stateDim = ndx.dimension(function (d) {
-        return d["school_state"];
+        return d["LEAGUE"];
     });
-    var fundingStatus = ndx.dimension(function (d) {
-        return d["funding_status"];
+    var focusSubjectDim = ndx.dimension(function(d) {
+        return d["PRICE DESCRIPTION"];
+    });
+
+    var povertyLevelDim = ndx.dimension(function (d) {
+        return d["POSITION"];
+    });
+
+    var totalDonationsDim = ndx.dimension(function (d) {
+        return d["TOTAL TRANSFERS"];
+    });
+    var all = ndx.groupAll();
+
+    var yearDim = ndx.dimension (function (d) {
+        return d["SEASON"];
     });
 
 
     //Calculate metrics
-    var numProjectsByDate = dateDim.group();
-    var numProjectsByResourceType = resourceTypeDim.group();
-    var numProjectsByPovertyLevel = povertyLevelDim.group();
     var numProjectsByFundingStatus = fundingStatus.group();
-    var totalDonationsByState = stateDim.group().reduceSum(function (d) {
-        return d["total_donations"];
-    });
+
     var stateGroup = stateDim.group();
 
+    var totalTransfers = ndx.groupAll().reduceSum(function (d) {
+        return d["PRICE"];
+    });
+    var numProjectsByFocusSubject =  focusSubjectDim.group();
 
-    var all = ndx.groupAll();
-    var totalDonations = ndx.groupAll().reduceSum(function (d) {
-        return d["total_donations"];
+    var numProjectsByPovertyLevel = povertyLevelDim.group();
+
+    var total_year = yearDim.group();
+
+    var valueDonationsByDate = dateDim.group().reduceSum(function (d) {
+        return d["PRICE"];
     });
 
+    var formatCommas = d3.format(",.0f");
+    var formatDollarsCommas = d3.format("$,.0f");
+
     //Define values (to be used in charts)
-    var minDate = dateDim.bottom(1)[0]["date_posted"];
-    var maxDate = dateDim.top(1)[0]["date_posted"];
+    var minYear = yearDim.bottom(1)[0]["SEASON"];
+    var maxYear = yearDim.top(1)[0]["SEASON"];
 
     //Charts
-    var timeChart = dc.lineChart("#time-chart");
-    var resourceTypeChart = dc.rowChart("#resource-type-row-chart");
-    var povertyLevelChart = dc.rowChart("#poverty-level-row-chart");
-    var numberProjectsND = dc.numberDisplay("#number-projects-nd");
-    var totalDonationsND = dc.numberDisplay("#total-donations-nd");
-    var fundingStatusChart = dc.pieChart("#funding-chart");
-    var selectField = dc.selectMenu('#menu-select');
+    var totalTransfersND = dc.numberDisplay("#total-donations-nd");
+    var transferWindowChart = dc.pieChart("#window-chart");
+    var transferTypeChart = dc.pieChart("#focus-subject-chart");
+    var playerPositionChart = dc.rowChart("#poverty-level-row-chart");
+    var numberTransfersND = dc.numberDisplay("#number-projects-nd");
+    var numberTransfersPerSeasonChart = dc.lineChart('#year_appearance');
+    var transferValueChart = dc.lineChart("#donation-value-line-chart");
 
 
-    selectField
+
+    selectField = dc.selectMenu('#menu-select')
         .dimension(stateDim)
         .group(stateGroup);
 
-    numberProjectsND
-        .formatNumber(d3.format("d"))
-        .valueAccessor(function (d) {
-            return d;
-        })
-        .group(all);
-
-    totalDonationsND
-        .formatNumber(d3.format("d"))
-        .valueAccessor(function (d) {
-            return d;
-        })
-        .group(totalDonations)
-        .formatNumber(d3.format(".3s"));
-
-    timeChart
-        .ordinalColors(["#C96A23"])
-        .width(1200)
-        .height(300)
-        .margins({top: 30, right: 50, bottom: 30, left: 50})
-        .dimension(dateDim)
-        .group(numProjectsByDate)
-        .renderArea(true)
-        .transitionDuration(500)
-        .x(d3.time.scale().domain([minDate, maxDate]))
-        .elasticY(true)
-        .xAxisLabel("Year")
-        .yAxis().ticks(6);
-
-
-    resourceTypeChart
-        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
-        .width(300)
-        .height(250)
-        .dimension(resourceTypeDim)
-        .group(numProjectsByResourceType)
-        .xAxis().ticks(4);
-
-    povertyLevelChart
-        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
-        .width(300)
-        .height(250)
-        .dimension(povertyLevelDim)
-        .group(numProjectsByPovertyLevel)
-        .xAxis().ticks(4);
-
-    fundingStatusChart
+    transferWindowChart
         .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
         .height(220)
         .radius(90)
@@ -126,6 +91,74 @@ function makeGraphs(error, donorsUSProjects) {
         .transitionDuration(1500)
         .dimension(fundingStatus)
         .group(numProjectsByFundingStatus);
+
+    totalTransfersND
+
+        .valueAccessor(function (d) {
+            return d;
+        })
+        .group(totalTransfers)
+        .formatNumber(formatDollarsCommas);
+
+
+    transferTypeChart
+        .height(220)
+        .radius(90)
+        .innerRadius(40)
+        .transitionDuration(1500)
+        .dimension(focusSubjectDim)
+        .group(numProjectsByFocusSubject);
+
+    playerPositionChart
+        .width(300)
+        .height(250)
+        .dimension(povertyLevelDim)
+        .group(numProjectsByPovertyLevel)
+        .xAxis().ticks(4);
+
+    numberTransfersND
+
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function (d) {
+            return d;
+        })
+        .group(all);
+
+     numberTransfersPerSeasonChart
+        .width(880)
+        .height(300)
+        .dimension(yearDim)
+        .group(total_year)
+        .x(d3.scale.ordinal().domain([(minYear),(maxYear)]))
+        .xUnits(dc.units.ordinal)
+        .elasticX(true)
+        .brushOn(false)
+        .renderArea(true)
+        .yAxisLabel("Number of Transfers")
+         .xAxisLabel("Season")
+        .colors(["#003d76"]);
+
+     transferValueChart
+        .width(880)
+        .height(300)
+         .margins(dateDimChartMargins)
+         .title(function (d) {
+            return d.key + ": " + formatCommas(d.value);
+        })
+        .dimension(yearDim)
+        .group(valueDonationsByDate, 'Total Transfer Fees (€)')
+        .x(d3.scale.ordinal().domain([(minYear),(maxYear)]))
+         .legend(dc.legend().x(120).y(20).itemHeight(13).gap(5))
+        .xUnits(dc.units.ordinal)
+         .mouseZoomable(false)
+        .elasticX(true)
+        .brushOn(false)
+        .renderArea(true)
+         .xAxisLabel("Season")
+        .ordinalColors(['#41ab5d'])
+     .elasticY(true)
+        .renderHorizontalGridLines(true);
+
 
 
     dc.renderAll();
