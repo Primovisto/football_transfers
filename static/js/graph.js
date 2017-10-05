eec5ade2903bde68e760a2f1d23d0e7ca94795c6
Primovisto <ed@velabri.co>
2017-10-05 21:21:22 +0100

    viewbox resizing 2

--- static/js/graph.js
+++ static/js/graph.js
@@ -139,7 +139,7 @@ function makeGraphs(error, donorsUSProjects) {
         .group(all);
 
      numberTransfersPerSeasonChart
-        .width(900)
+
         .height(300)
         .dimension(yearDim)
         .group(total_year)
@@ -156,7 +156,7 @@ function makeGraphs(error, donorsUSProjects) {
 
 
      transferValueChart
-        .width(900)
+
         .height(300)
          .margins(dateDimChartMargins)
          .title(function (d) {
