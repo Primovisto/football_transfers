f7bf74ae413e118a541514f8edd25621e31b09ff
Primovisto <ed@velabri.co>
2017-10-11 11:14:46 +0100

    added viewbox resizing, fixed rogue css issue and new barchart

--- static/js/graph.js
+++ static/js/graph.js
@@ -13,9 +13,6 @@ function makeGraphs(error, donorsUSProjects) {
     var ndx = crossfilter(donorsUSProjects);
 
     //Define Dimensions
-    var dateDim = ndx.dimension(function (d) {
-        return d["SEASON"];
-    });
 
     var dateDimChartMargins = {top: 30, right: 50, bottom: 25, left: 90};
 
@@ -25,20 +22,16 @@ function makeGraphs(error, donorsUSProjects) {
     var stateDim = ndx.dimension(function (d) {
         return d["LEAGUE"];
     });
-    var focusSubjectDim = ndx.dimension(function(d) {
+    var focusSubjectDim = ndx.dimension(function (d) {
         return d["PRICE DESCRIPTION"];
     });
 
     var povertyLevelDim = ndx.dimension(function (d) {
         return d["POSITION"];
     });
-
-    var totalTransfersDim = ndx.dimension(function (d) {
-        return d["TOTAL TRANSFERS"];
-    });
     var all = ndx.groupAll();
 
-    var yearDim = ndx.dimension (function (d) {
+    var dateDim = ndx.dimension(function (d) {
         return d["SEASON"];
     });
 
@@ -51,11 +44,11 @@ function makeGraphs(error, donorsUSProjects) {
     var totalTransfers = ndx.groupAll().reduceSum(function (d) {
         return d["PRICE"];
     });
-    var numProjectsByFocusSubject =  focusSubjectDim.group();
+    var numProjectsByFocusSubject = focusSubjectDim.group();
 
     var numProjectsByPovertyLevel = povertyLevelDim.group();
 
-    var total_year = yearDim.group();
+    var total_year = dateDim.group();
 
     var valueDonationsByDate = dateDim.group().reduceSum(function (d) {
         return d["PRICE"];
@@ -65,28 +58,28 @@ function makeGraphs(error, donorsUSProjects) {
     var formatDollarsCommas = d3.format("$,.0f");
 
     //Define values (to be used in charts)
-    var minYear = yearDim.bottom(1)[0]["SEASON"];
-    var maxYear = yearDim.top(1)[0]["SEASON"];
+    var minDate = dateDim.bottom(1)[0]["SEASON"];
+    var maxDate = dateDim.top(1)[0]["SEASON"];
+
 
     //Set Colours
 
     var pieChartColours = ['#d11141', '#00b159', '#00aedb', '#ffc425'];
-    var rowChartColours =['#d11141', '#00b159', '#00aedb', '#ffc425'];
+    var rowChartColours = ['#d11141', '#00b159', '#00aedb', '#ffc425'];
 
 
     //Charts
+    var numberTransfersPerSeasonChart = dc.barChart("#year_appearance");
     var totalTransfersND = dc.numberDisplay("#total-transfers-nd");
     var transferWindowChart = dc.pieChart("#window-chart");
     var transferTypeChart = dc.pieChart("#transfer-type-chart");
     var playerPositionChart = dc.rowChart("#player-position-row-chart");
     var numberTransfersND = dc.numberDisplay("#number-transfers-nd");
-    var numberTransfersPerSeasonChart = dc.lineChart('#year_appearance');
     var transferValueChart = dc.lineChart("#donation-value-line-chart");
 
     selectField = dc.selectMenu('#menu-select')
         .dimension(stateDim)
         .group(stateGroup);
-
     transferWindowChart
         .ordinalColors(pieChartColours)
         .height(234)
@@ -95,17 +88,16 @@ function makeGraphs(error, donorsUSProjects) {
         .transitionDuration(1500)
         .dimension(fundingStatus)
         .group(numProjectsByFundingStatus)
-        .useViewBoxResizing(true);
-
+        .useViewBoxResizing(true)
+       ;
     totalTransfersND
         .valueAccessor(function (d) {
             return d;
         })
         .group(totalTransfers)
         .formatNumber(formatDollarsCommas)
-        .useViewBoxResizing(true);
-
-
+    .useViewBoxResizing(true)
+        ;
     transferTypeChart
         .height(234)
         .radius(100)
@@ -118,64 +110,61 @@ function makeGraphs(error, donorsUSProjects) {
         .legend(dc.legend().x(20).y(10).itemHeight(13).gap(5))
         .cx(220)
         .cy(117)
-        .useViewBoxResizing(true);
-
+        .useViewBoxResizing(true)
+        ;
     playerPositionChart
         .width(300)
         .height(250)
         .dimension(povertyLevelDim)
         .group(numProjectsByPovertyLevel)
         .ordinalColors(rowChartColours)
-        .xAxis().ticks(4);
-
-
+        .xAxis().ticks(6);
     numberTransfersND
         .formatNumber(d3.format("d"))
         .valueAccessor(function (d) {
             return d;
         })
         .group(all)
-        .useViewBoxResizing(true);
-
-     numberTransfersPerSeasonChart
+        .useViewBoxResizing(true)
+        ;
+    numberTransfersPerSeasonChart
         .width(900)
-        .height(300)
-        .dimension(yearDim)
-        .group(total_year)
-        .x(d3.scale.ordinal().domain([(minYear),(maxYear)]))
+        .height(325)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dateDim)
+        .group(total_year, 'Number of Transfers')
+        .x(d3.scale.ordinal().domain([(minDate), (maxDate)]))
         .xUnits(dc.units.ordinal)
         .elasticX(true)
-        .brushOn(false)
-        .renderArea(true)
-         .xAxisLabel("Season")
-        .yAxisLabel("Number of Transfers")
-         .xAxisLabel("Season")
-        .colors(["#00b159"])
-        .useViewBoxResizing(true);
+        .transitionDuration(500)
+        .xAxisLabel("Season")
+        .elasticY(true)
+        .useViewBoxResizing(true)
+        ;
 
-
-
-     transferValueChart
-         .width(900)
-         .height(300)
-         .margins(dateDimChartMargins)
-         .title(function (d) {
+    transferValueChart
+        .width(900)
+        .height(325)
+        .margins(dateDimChartMargins)
+        .title(function (d) {
             return d.key + ": " + formatCommas(d.value);
         })
-         .dimension(yearDim)
-         .group(valueDonationsByDate, 'Total Transfer Fees ($)')
-         .x(d3.scale.ordinal().domain([(minYear),(maxYear)]))
-         .legend(dc.legend().x(120).y(20).itemHeight(13).gap(5))
-         .xUnits(dc.units.ordinal)
-         .elasticX(true)
-         .renderArea(true)
-         .yAxisLabel("$")
-         .xAxisLabel("Season")
-         .ordinalColors(['#00b159'])
-         .elasticY(true)
-         .useViewBoxResizing(true);
+        .dimension(dateDim)
+        .group(valueDonationsByDate, 'Total Transfer Fees ($)')
+        .x(d3.scale.ordinal().domain([(minDate), (maxDate)]))
+        .legend(dc.legend().x(120).y(20).itemHeight(13).gap(5))
+        .xUnits(dc.units.ordinal)
+        .elasticX(true)
+        .renderArea(true)
+        .renderHorizontalGridLines(true)
+        .yAxisLabel("$")
+        .xAxisLabel("Season")
+        .ordinalColors(['#00b159'])
+        .elasticY(true)
+        .useViewBoxResizing(true)
+        ;
+
 
     dc.renderAll();
-
-
 }
+
